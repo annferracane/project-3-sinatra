ed1e873bbe062205a0199f8a3fd398c9bf60e59d
Dereck Cervantes <dereckfer51@gmail.com>
2023-01-23 17:46:02 -0500

    second commit

--- phase-3-project/src/App.js
+++ phase-3-project/src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
 
 function App() {
+
+const [reviews, setReviews] = useState([])
+
+useEffect(() =>{
+fetch("http://localhost:9292/reviews")
+.then(response => response.json())
+.then(setReviews)
+
+},[])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reac
-        </a>
-      </header>
-    </div>
-  );
+<div>
+
+</div>
+  )
 }
 
 export default App;
