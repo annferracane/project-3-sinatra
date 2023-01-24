6fec8bd5bbb5ec7dafcb2e1e3fc4708c8e2581fe
Ann Ferracane <ann@patchventures.com>
2023-01-24 14:40:52 -0500

    new review

--- phase-3-project/src/components/NewReview.js
+++ phase-3-project/src/components/NewReview.js
@@ -1,56 +1,36 @@
 import React, { useState } from 'react';
-// import { useHistory } from 'react-router-dom';
+//import { useHistory } from 'react-router-dom';
 
-function NewReview({  }) { 
-    
-const [title, setTitle] = useState('');
-const [image, setImage] = useState('');
-const [comment, setComment] = useState('');
+function NewReview({ addNewReview }) {
+    console.log(addNewReview)
+    const [title, setTitle] = useState('');
+    const [image, setImage] = useState('');
+    const [comment, setComment] = useState('');
+    //const history = useHistory();
 
-// const history = useHistory();
+    function handleSubmit(e) {
 
-function handleSubmit(e) { 
-    e.preventDefault();
+        e.preventDefault();
 
-    // const newDate = new Date();
-    // const currentDate = newDate.getDate().toString();
-    // const month = newDate.getMonth() + 1;
-    // const currentMonth = month.toString();
-    // const currentYear = newDate.getFullYear().toString();
-
-    // const newComic = { 
-    //     title: title, 
-    //     img: image, 
-    //     alt: comment,
-    //     month: currentMonth,
-    //     link: "",
-    //     year: currentYear,
-    //     news: "",
-    //     safe_title: title,
-    //     transcript: comment,
-    //     alt: comment,
-    //     day: currentDate,
-    //     favorited: false,
-    //     comments: [],
-    //     alt: comment 
-    // };
-
-    // fetch(`http://localhost:3001/jokes`, {
-    //   method: "POST",
-    //   headers: {
-    //     "Content-Type": "application/json",
-    //   },
-    //   body: JSON.stringify(newComic),
-    // })
-    // .then(resp => resp.json())
-    // .then(data => {
-    //     handleNewComic(data);
-    //     history.push(`/comics/${data.id}`);
-    // });
-  
-}
-
-    return ( 
+        const newReview = {
+            title: title,
+            image: image,
+            comment: comment
+        }
+        fetch(`http://localhost:9292/reviews`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(newReview),
+        })
+        .then(resp => resp.json())
+        .then(mynewReview => {
+            addNewReview(mynewReview);
+            //history.push(`/cereals/${mynewReview.id}`);
+        });
+    }
+    return (
         <div className="container py-4">
             <div className="row align-items-md-stretch">
                 <div className="p-5 text-bg-dark rounded-3">
@@ -64,12 +44,12 @@ function handleSubmit(e) {
                             <input type="text" className="form-control" id="image" value={image} onChange={(e) => setImage(e.target.value)} />
                         </div>
                         <div className="form-group">
-                            <label htmlFor="comments">Comic's Note</label>
+                            <label htmlFor="comments">Comment</label>
                             <textarea className="form-control" id="exampleFormControlTextarea1" rows="3" value={comment} onChange={(e) => setComment(e.target.value)}></textarea>
                         </div>
                         <br></br>
                         <button type="submit" className="btn btn-primary">Submit</button>
-                    </form> 
+                    </form>
                 </div>
             </div>
         </div>
